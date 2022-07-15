f38653c9523eb3325837587974f8861d0d33d7e5
marta0220 <109087240+marta0220@users.noreply.github.com>
2022-07-15 13:40:47 +0300

    Removing today from the forecast

--- src/index.js
+++ src/index.js
@@ -96,7 +96,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 6 && index !== 0) {
       forecastHTML =
         forecastHTML +
         `<div class="col-3"><img src=img/${
